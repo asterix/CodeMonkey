a8d4e049151b9094f9fd7993ebd8f64d8ae9fd0d
desai043 <desai043@umn.edu>
2016-06-16 02:49:45 -0500

    2016-Round-A-C

--- gCampus-DijkstrasShortestPath/gcampus.cpp
+++ gCampus-DijkstrasShortestPath/gcampus.cpp
@@ -14,22 +14,100 @@ using namespace std;
 
 struct graph
 {
-   unsigned int g[100][100];
+   unsigned int *g;
+   unsigned int *d;
+   int sz;
+
+   vector<vector<pair<int,int> > > p;
 
    // Clear graph
    void clear()
    {
-      for(int a = 0; a < 100; a++)
-         for(int b = 0; b < 100; b++)
-            g[a][b] = -1;
+      for(int a = 0; a < sz * sz; a++)
+         g[a] = -1;
+      for(int a = 0; a < sz; a++)
+         g[a*sz+a] = 0;
+   }
+
+   void cleard()
+   {
+      for(int a = 0; a < sz; a++)
+         d[a] = -1;
+   }
+
+   void clearp()
+   {
+      for(auto i = p.begin(); i != p.end(); i++)
+         i->clear();
+   }
+
+   graph() = delete;
+   graph(int s)
+   {
+      sz = s;
+      g = new unsigned int [sz * sz];
+      d = new unsigned int [sz];
+
+      for(int i = 0; i < sz; i++)
+      {
+         vector<pair<int,int> > x;
+         p.push_back(x);
+      }
+   }
+
+   ~graph()
+   {
+      delete[] g;
+      delete[] d;
    }
 };
 
 
-void dijkstra_sps(int a, graph *g, vector<pair<int, int> > *p)
+int search_min(graph &g, vector<int> &v)
 {
+   unsigned int min = -1;
+   int min_idx = -1;
+   for(int i = 0; i < g.sz; i++)
+   {
+      if((g.d[i] < min) &&
+         (find(v.begin(), v.end(), i) == v.end()))
+      {
+         min = g.d[i];
+         min_idx = i;
+      }
+   }
 
+   return min_idx;
 }
+
+
+void dijkstra_sps(int a, graph &g)
+{
+   g.cleard(); g.clearp();
+   g.d[a] = 0;
+
+   vector<int> v; v.clear();
+
+   while(v.size() < g.sz)
+   {
+      int min = search_min(g, v);
+      v.push_back(min);
+
+      for(int i = 0; i < g.sz; i++)
+      {
+         if(g.g[min*g.sz+i] != -1)
+         {
+            if(g.d[i] > (g.d[min] + g.g[min*g.sz+i]))
+            {
+               g.d[i] = g.d[min] + g.g[min*g.sz+i];
+               g.p.at(i) = g.p.at(min);
+               g.p.at(i).push_back(make_pair(min, i));
+            }
+         }
+      }
+   }
+}
+
 
 int main()
 {
@@ -41,9 +119,7 @@ int main()
    int num_tests;
    infile >> num_tests;
 
-   graph g, e;
-   vector<pair<int, int> > p;
-   vector<tuple<pair<int, int>, bool> > ed;
+   vector<tuple<int, int, unsigned int> > ed, ef;
    
    for(int i = 0; i < num_tests; i++)
    {
@@ -52,40 +128,43 @@ int main()
       int n, m, u, v;
       unsigned int w;
       infile >> n >> m;
+      graph g(n), e(n);
 
-      g.clear(); e.clear(); p.clear();
+      g.clear(); e.clear(); ed.clear(); ef.clear();
 
       // Create graph - mark efficient paths
       for(int a = 0; a < m; a++)
       {
          infile >> u >> v >> w;
-         if(g.g[u][v] > w)
+         if(g.g[u*g.sz+v] > w)
          {
-            g.g[u][v] = w; g.g[v][u] = w;
-            e.g[u][v] = a; e.g[v][u] = a;
+            g.g[u*g.sz+v] = w; g.g[v*g.sz+u] = w;
          }
-         ed.push_back(make_tuple(make_pair(u, v), true));
+         ed.push_back(make_tuple(u, v, w));
       }
 
       // Search shortest paths
       for(int a = 0; a < n; a++)
       {
          // a -> all shortest paths
-         dijkstra_sps(a, &g, &p);
+         dijkstra_sps(a, g);
 
-         // Mark shortest paths as efficient
-         for(int h = 0; h < ed.size(); h++)
+         // Save efficient paths
+         for(auto q = g.p.begin(); q != g.p.end(); q++)
          {
-            if(find(p.begin(), p.end(), get<0>(ed.at(h))) != p.end())
+            for(auto c = q->begin(); c != q->end(); c++)
             {
-               if(e.g[get<0>(ed.at(h)).first][get<0>(ed.at(h)).second] == h)
-               {
-                  get<1>(ed.at(h)) = false;
-               }
+               ef.push_back(make_tuple(c->first, c->second, g.g[c->first*g.sz+c->second]));
             }
          }
       }
-      
+
+      // Write inefficient paths out
+      for(int a = 0; a < ed.size(); a++)
+      {
+         if(find(ef.begin(), ef.end(), ed.at(a)) == ef.end())
+            outfile << a << endl;
+      }
    }
 
 
